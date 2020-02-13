0307ae094d4f34cb4edf53c110ec8ffe12637fb8
AlphabetJ <58789319+AlphabetJ@users.noreply.github.com>
2020-02-13 16:21:31 +0700

    jate 1st

---
+++ chatz/src/components/chatz.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <div v-if="state == 0">
+            <h3>Welcome! Please choose a username.</h3>
+            <form @submit.prevent="setUsername">
+                <input type="text" placeholder="Username..." v-model="username" />
+                <input type="submit" value="Join" />
+            </form>
+            <button @click="continueWithoutUsername"> Join as a Guest </button>
+        </div>
+        <div v-if="state == 1">
+            <ul id="chatbox">
+                <li v-for="message in messages" :key="message.user">
+                    <b>{{ message.user }}:</b> {{ message.message }}
+                </li>
+            </ul>
+            <form @submit.prevent="sendMessage">
+                <input type="text" placeholder="Message..." v-model="message" />
+                <input type="submit" value="Send" />
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'chatz',
+    
+}
+
+</script>
---
+++ client/src/main.ts
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+import firebase from 'firebase';
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import {firebaseConfig} from './_config_ci/firebaseKey'
+import '@/components/Login/loginstyle.scss'
+import '@/components/Login/mixin.scss'
+import '@/components/Login/acceptstyle.scss'
+import '@/components/Login/forgetstyle.scss'
+import '@/components/Login/regisstyle.scss'
+import '@/assets/bglogin.png'
+Vue.config.productionTip = false
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+import axios from "axios";
+/* eslint-disable */
+const client = axios.create({
+  baseURL: "http://localhost:5001/relint-kmitl/us-central1/app",
+  // baseURL: "https://us-central1-relint-kmitl.cloudfunctions.net/app",
+});
+Vue.prototype.$http = client
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
