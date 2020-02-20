7efb4ce36569597219dc1846eebe7d46ef51fde8
fairickoo <46299109+fairickoo@users.noreply.github.com>
2020-02-20 16:55:50 +0700

    commit navbar

--- client/src/components/createBoard/addBoard.vue
+++ client/src/components/createBoard/addBoard.vue
@@ -1,29 +1,27 @@
-<template> <div>
-    <b-navbar toggleable="lg" type="light" >
-        <b-navbar-brand >Relint</b-navbar-brand>
-      
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav>
-          <!-- Left aligned nav items -->
+<template> 
+<div>
+  <b-navbar toggleable="lg" type="light" >
+    <b-navbar-brand >Relint</b-navbar-brand>
 
-            <b-navbar-nav>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <!--menu-->
+    <b-collapse id="nav-collapse" is-nav>
+       <b-navbar-nav>
               <b-nav-item ><b-icon icon="house" font-scale="3"></b-icon></b-nav-item>
               <b-nav-item  ><b-icon icon="kanban" font-scale="3"></b-icon></b-nav-item>
-            </b-navbar-nav>
-
-          
-          <!-- Right aligned nav items -->
-          <!--Search-->
+        </b-navbar-nav>
+        
+          <!--search-->
           <b-navbar-nav class="ml-auto">
             <b-nav-form>
               <b-form-input size="l" class="mr-sm-2"  placeholder="Search"></b-form-input>
               <b-button size="sm" class="my-2 my-sm-0"  ><b-icon icon="search" font-scale="1.8"  ></b-icon></b-button>
             </b-nav-form>
 
-            <!--User-->
+            <!--user-->
             <b-nav-item-dropdown right >
               <template v-slot:button-content  >
-                <b-icon icon="person" font-scale="3" v-on:click="showName" ></b-icon>
+                <b-icon icon="person" font-scale="3"  ></b-icon>
               </template>
                
               <b-dropdown-header> 
@@ -31,17 +29,14 @@
                 <b-icon icon="star" font-scale="1.3"></b-icon>
                 <b-dropdown-divider></b-dropdown-divider>
               </b-dropdown-header>
-               
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
               
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
               <b-dropdown-item  v-on:click="logout">Sign Out</b-dropdown-item>
             </b-nav-item-dropdown>
-
           </b-navbar-nav>
-          
-        </b-collapse>
-    </b-navbar> 
-    </div>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 <script  >
 /* eslint-disable */
