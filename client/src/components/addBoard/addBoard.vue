2d6f2f2713041584f71478b343508caa3603325c
fairickoo <61010656@kmitl.ac.th>
2020-03-17 23:27:23 +0700

    edit style board & scroll bar to left

--- client/src/components/addBoard/addBoard.vue
+++ client/src/components/addBoard/addBoard.vue
@@ -2,7 +2,7 @@
     <div>
        <navBar/>
         <!-- <test/>  -->
-        <!-- <temp/> -->
+        
       <createBoard/>
     </div>
 </template>
@@ -10,16 +10,16 @@
 /* eslint-disable */
 import navBar from '../navBar/navBar'
 import createBoard from '../createBoard/createBoard'
-import temp from '../createBoard/temp'
-import test from '../createBoard/test'
+
+// import test from '../createBoard/test'
 
 export default {
     name: 'addBoardPostit',
       components: {
         navBar,
         createBoard,
-        test,
-        temp
+        // test,
+       
     }
 }
 </script>
