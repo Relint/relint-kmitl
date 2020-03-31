a2e2118d9bd2962d8da39ed1e871588b56f547aa
fairickoo <61010656@kmitl.ac.th>
2020-03-31 12:31:43 +0700

    debug wait I

--- client/src/components/Login/login.vue
+++ client/src/components/Login/login.vue
@@ -2,12 +2,14 @@
 <!-- login -->
 <div>
   <title>Relint</title>
+  
   <div class="mainGroup" id="login" >
             <div class="navbarLogin">
               <li class="brand">RELINT</li>
             </div>
-               
-         
+           <div id="waiting-login">
+           <waiting/>
+          </div>
         <div class="parentbg">
           <div class="div1-bg">
           <div class="parentlogo">
@@ -21,9 +23,9 @@
               <div class="div6-lo"> </div>
               <div class="div7-lo"></div>
              <label class="logoName">RELINT</label>
+             
           </div>
-          </div>
-          
+          </div> 
         <div class="div1-dot"> 
            <div  class="form-container"   > 
                   <div class="dotBG1"></div>
@@ -32,6 +34,7 @@
                   <div class="dotBG4"></div>
             </div> 
         </div>
+          
         <!-- login -->
         <div class="div2-bg">
           <div class="from-login" id='from-login'>
@@ -61,7 +64,7 @@
             </div>
 
             <!-- forget password -->
-             
+            
             <div class="form-popup" id="forget-from">
               <div class="div1-login">
                   <div id="alert-error-for" > {{errorMessageFor}}</div>
@@ -110,6 +113,7 @@
      
       
       </div>
+       
 </div>   
   
    
@@ -118,8 +122,12 @@
 </template>
 <script>
 import firebase from "firebase"
+import waiting from '../wait/waiting'
 export default {
   name: 'LoginPage',
+  components: {
+    waiting
+  },
   beforeCreate () {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
@@ -133,9 +141,12 @@ export default {
     document.getElementById('alert-error-login').style.display = "none"
     document.getElementById('alert-error').style.display = "none"
     document.getElementById('form-register').style.display = "none"
+    document.getElementById('waiting-login').style.display = "none"
+    
   },
   data: function () {
     return {
+      stateCheck:false,
       timeout:3000,
       errorMessageFor:'',
       errorMessageLogin:'',
@@ -154,11 +165,9 @@ export default {
     //forget
     closeFormFOR() {
       document.getElementById("forget-from").style.display = "none";
-      
     },
     openFormFOR() {
       document.getElementById("forget-from").style.display = "block";
-      
     }, 
     //acceptSEnd
     acceptSend(e) { 
@@ -166,6 +175,7 @@ export default {
             document.getElementById('alert-error-for').style.display = "none"
       }, this.timeout);
       document.getElementById('alert-error-for').style.display = "block"
+
       firebase.auth()
         .sendPasswordResetEmail(this.emailpWS).then(() => {
           this.errorMessageFor= 'Password reset email sent'
@@ -190,8 +200,9 @@ export default {
     },
     //anth
     login(e) {
-      if((!this.email.match(/^([\x20-\x7E])+$/i))||(!this.password.match(/^([\x20-\x7E])+$/i)) )
-        { 
+      const alertErrorLogin = document.getElementById('alert-error-login') 
+      const alertWaiting = document.getElementById('waiting-login')
+      if((!this.email.match(/^([\x20-\x7E])+$/i))||(!this.password.match(/^([\x20-\x7E])+$/i))){
           if (!this.email.match(/^([\x20-\x7E])+$/i)) {
               this.errorMessageLogin='Invalid email'
               this.email=''
@@ -200,21 +211,20 @@ export default {
               this.errorMessageLogin='Invalid password'
               this.password=''
            }
-          setTimeout(function(){
-                    document.getElementById('alert-error-login').style.display = "none"
-          }, this.timeout);
-          document.getElementById('alert-error-login').style.display = "block"
-          return
-        }
-        setTimeout(function(){
-                    document.getElementById('alert-error-login').style.display = "none"
-        }, this.timeout);
-        document.getElementById('alert-error-login').style.display = "block"
-
+          if (alertErrorLogin){
+              setTimeout(() => {
+              alertErrorLogin.style.display = "none"
+            },this.timeout)
+            alertErrorLogin.style.display = "block"
+            return 
+          }
+      }
+      else {
         firebase.auth()
         .signInWithEmailAndPassword(this.email, this.password)
         .then(() => {
-          this.regis = false;
+           this.regis = false;
+          // this.errorMessageLogin='Authentication Completed'
           // alert('Authentication Completed');
         })
         .catch(err => {
@@ -222,6 +232,16 @@ export default {
           // alert(err);
         });
       e.preventDefault();
+      if (alertErrorLogin){
+              setTimeout(() => {
+              alertErrorLogin.style.display = "none"
+            },this.timeout)
+            alertErrorLogin.style.display = "block"
+           }
+      }
+      if (alertWaiting){
+      alertWaiting.style.display = "block"
+      }
     },
     register(e) {  
       if((!this.emailRE.match(/^([\x20-\x7E])+$/i))||(!this.passwordRE.match(/^([\x20-\x7E])+$/i))||
