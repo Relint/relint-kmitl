e44b9d394dda8ba6392a8d686fb52f64c9fc9287
fairickoo <61010656@kmitl.ac.th>
2020-04-08 01:24:05 +0700

    temp postit

--- client/src/components/boardPostit/boardPostit.vue
+++ client/src/components/boardPostit/boardPostit.vue
@@ -3,36 +3,43 @@
     <div class="contain-boardPostit">
       <div class="form-scroll-postit" id="postit-scroll">
         <div class="from-boardPostit">
-          <div class="add-postit-begin" id="begin-postit">
-            <input id="begin-postit" type="text" placeholder="add postit" v-model="postItIn" />
-            <button @click="createPostit">Ok</button>
-          </div>
+          
+            <div v-for="(postit , index) in postits" :key="postit.postId" ref="postId">
+                <div class="form-show-postit" v-bind:style="{left: 10+(index)*350+(index)*30 + 'px',top:0+'px'  }">
+                      {{postit.title}}
+                      <button @click="editPostit(postit.postId)"   >...</button>
+                      <button @click="deletePostit(postit.postId)">delete</button><br/>
+                      <button>add card</button>
+                </div>
+                
+                <div v-if="postits.length>0">
+                    <div class="form-input-postit" v-bind:style="{left:(postits.length)*350+(postits.length)*30 + 'px',top:0+'px'  }">
+                      <input class="input-postit" type="text" placeholder="add post-it" v-model="postitIn">
+                      <div @click="createPostit(postit.postId)" class="btn-create-postit"><b-icon class="center-icon" icon="plus" font-scale="3" ></b-icon></div>
+                      <!-- <button @click="createPostit2" >test</button> -->
+                    </div>
+                </div>
+                <div class="form-edit-postit" id="form-edit-postit"  v-bind:style="{left: 10+(index)*350+(index)*30 + 'px',top:50+'px'  }">
+                  <input class="input-edit-postit" type="text" placeholder="add post-it" v-model="postitIn">
+                  <button>save</button>
+                  <button>cancel</button>
+                </div>
+            </div>
+            <div class="form-input-postit" id="form-input-postit" >
+                <input class="input-postit" type="text" placeholder="add post-it" v-model="postitIn">
+                <div @click="createPostit" class="btn-create-postit"><b-icon class="center-icon" icon="plus" font-scale="3" ></b-icon></div>
+            </div>
+            
+             <!-- <div v-for="card in cards" :key="card.cid">
+                  <div class="aCard">
+                    <cardBox :cardName="cardName" :date="date"/>
+                  </div>
+              </div> -->
 
-          <div v-for="(postIt,index) in postIts" :key="postIt.postId">
-            <div
-              id="show-postit "
-              class="show-postit"
-              v-bind:style="{left:10+(index)*350+(index)*30 + 'px',top:30+'px' , }"
-            >
-              <div class="form-postit">{{index}} {{postIt}}</div>
-            </div>
-            <div
-              class="next-postit"
-              v-bind:style="{left:400+(index)*350+(index)*30 + 'px',top:30+'px' , }"
-            >
-              <div class="form-next-input-postit">
-                <input type="text" placeholder="add postit" v-model="postItIn" />
-                <button @click="createPostit">Ok</button>
-              </div>
-            </div>
-            <div class="aCard">
-              <cardBox :cardName="cardName" :date="date"/>
-            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 </template>
 <script  >
 /* eslint-disable */
@@ -41,29 +48,91 @@ import {
   isOfflineForDatabase,
   isOnlineForDatabase
 } from "../../db presets/presets";
-import cardBox from "../boardPostit/cardBox";
-//import navBar from '../navBar/navBar'
+
 export default {
   name: "boardPostit",
   components: {
-    cardBox
   },
   data() {
     return {
-      postIts: [],
-      postItIn: ""
+      postitIn:'',
+      postits:[],
+      temp0:[
+        {
+          pid:'P1',
+          postId:'Po1',
+          title:'test1',
+        },
+        {
+          pid:'P1',
+          postId:'Po2',
+          title:'test2',
+        },
+        {
+          pid:'P1',
+          postId:'Po3',
+          title:'test3',
+        },
+        {
+          pid:'P1',
+          postId:'Po4',
+          title:'test4',
+        },
+        {
+          pid:'P1',
+          postId:'Po5',
+          title:'test5',
+        }
+      ]
     };
   },
+  mounted () {
+    document.getElementById('form-edit-postit').style.display='none'
+  },
   methods: {
-    createPostit() {
-      this.postIts.push(this.postItIn);
-      this.postItIn = "";
-      document.getElementById("begin-postit").style.display = "none";
-      // document.getElementById('input-postit').style.display='none'
-      // document.getElementById('form-input-postit').classList.add('show-postit')
+    deletePostit (postId) {
+       this.postits = this.postits.filter(postit => {
+         return postit.postId !== postId
+         })
+    },
+    editPostit (postId) {
+      console.log(this.$refs.postId)
+        // if (document.getElementById('form-edit-postit').style.display==='none'){
+        //   document.getElementById('form-edit-postit').style.display='block'
+        // }
+        // else {
+        //   document.getElementById('form-edit-postit').style.display='none'
+        // }
+     
+      
+      //    if (document.getElementById('form-edit-postit').style.display==='none'){
+      //   document.getElementById('form-edit-postit').style.display='block'
+      // }
+      // else {
+      //   document.getElementById('form-edit-postit').style.display='none'
+      // }
+     
+
+      // if (document.getElementById('form-edit-postit').style.display==='none'){
+      //   document.getElementById('form-edit-postit').style.display='block'
+      // }
+      // else {
+      //   document.getElementById('form-edit-postit').style.display='none'
+      // }
+      
+    },
+    // createPostit2 () {
+    //   this.postits.push(this.postitIn)
+    // },
+    createPostit () {
+      this.postits=[]
+      this.temp0.forEach(element => {
+        this.postits.push(element)
+      });
+      document.getElementById('form-input-postit').style.display='none'
     }
   }
-};
+}
 </script>
 <style scoped lang="scss">
 @import "./boardPostitStyle.scss";
