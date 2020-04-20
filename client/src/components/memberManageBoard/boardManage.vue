263adbd12a4e21f98313ea45741be9182fd7c490
fairickoo <61010656@kmitl.ac.th>
2020-04-20 16:15:51 +0700

    css member tap $ invite tap

--- client/src/components/memberManageBoard/boardManage.vue
+++ client/src/components/memberManageBoard/boardManage.vue
@@ -46,43 +46,46 @@
             </div>
           </div>
         </div>
-        <div class="stage-r">
+        <div  class="stage-r">
           <div v-if="project">
             
-              <div class="member-tab" @click="toggleDropMember">Members ({{project.member.length}})</div>
+              <div class="member-tab" @click="toggleDropMember">Members ({{project.member.length}}) <b-icon icon="caret-down" shift-v="-4"></b-icon></div>
               <div class="scroll-member">
-              <div id="showMem">
+              <div v-if="project.member.length" id="showMem">
                 <div v-for="(member,index) in project.member" :key="'member'+index">
-                  <div class="userBox-parent">
+                  <div   class="userBox-parent" id="userBox-parent">
                     <div class="userName">{{ analysisSender('name',member.uid)}}</div>
                     <div class="email">{{analysisSender('email',member.uid)}}</div>
                     <div @click="dropEditPermission(member.uid,index)" id="pri" class="priority noselect cursorPointer" v-if="project.member.filter(ele=>ele.uid===member.uid)[0] && analysisSender('permission',member.uid) !==0 && member.uid != $store.state.uid && project.permission===0">{{priorityMap[project.member.filter(ele=>ele.uid===member.uid)[0].priority]}}<b-icon icon="caret-down" shift-v="-4"></b-icon></div>
                     <div id="pri" class="priority noselect" v-else-if="project.member.filter(ele=>ele.uid===member.uid)[0]">{{priorityMap[project.member.filter(ele=>ele.uid===member.uid)[0].priority]}}</div>
-                    <div class="drop-permission noselect">
+                    <div  class="drop-permission noselect">
                       <div v-if="analysisSender('permission',member.uid) == 1">
+                        <div class="closeX " @click="dropEditPermission(member.uid,index)">x</div><br> 
                         <a @click="selPri(member.uid,index,1)"><div class="bold ">Co-Admin</div></a>
                         <a @click="selPri(member.uid,index,2)">Member</a>
                       </div>
                       <div v-else-if="analysisSender('permission',member.uid) == 2">
+                        <div class="closeX " @click="dropEditPermission(member.uid,index)">x</div><br> 
                         <a @click="selPri(member.uid,index,2)"><div class="bold">Member</div></a>
                         <a @click="selPri(member.uid,index,1)">Co-admin</a>
                       </div>
                     </div>
                     <br class="noselect">
-                    <button v-if="analysisSender('permission',member.uid) > project.permission" class="removeBtn" @click="removeMember(member.uid)">remove</button>
-                    <button v-else-if="member.uid===$store.state.uid && project.permission!=0" class="removeBtn" @click="leave(member.uid)">leave</button> 
+                    <button v-if="analysisSender('permission',member.uid) > project.permission" class="removeBtn" @click="removeMember(member.uid)">Remove</button>
+                    <button v-else-if="member.uid===$store.state.uid && project.permission!=0" class="removeBtn" @click="leave(member.uid)">Leave</button> 
                   </div>
                 </div>
                 
               </div>
               
-              <div class="invite-tab" @click="toggleDropInvite">Invites ({{project.invite.length}})</div>
-              <div id="showInvite">
+              <div  class="invite-tab" @click="toggleDropInvite">Invites ({{project.invite.length}}) <b-icon icon="caret-down" shift-v="-4"></b-icon></div>
+              <div v-if="project.invite.length" id="showInvite">
                 <div v-for="(member,index) in project.invite" :key="'invite'+index">
-                  <div class="userBox-parent">
+                  <div  class="userBox-parentIn">
+                    <div class="userName">{{ analysisSender('name',member.uid)}}</div>
                     <div class="email" v-if="users.filter(ele=>ele.uid===member.uid)[0]">{{users.filter(ele=>ele.uid===member.uid)[0].email}}</div>
                     <div  class="priority" v-if="project.invite.filter(ele=>ele.uid===member.uid)[0]">{{priorityMap[project.invite.filter(ele=>ele.uid===member.uid)[0].priority]}}</div>
-                    <button v-if="project.permission!=2 ||member.uid===$store.state.uid" class="removeBtn" @click="removeInvite(member.uid)">remove</button>
+                    <button v-if="project.permission!=2 ||member.uid===$store.state.uid" class="removeBtn topBtn" @click="removeInvite(member.uid)">Remove</button>
                   </div>
                 </div>
               </div>
@@ -95,7 +98,7 @@
                     {{ opt.status }}
                 </option>
               </select><br class="noselect">
-              <button  class="btn-invite-ok noselect" @click="addMember">ok</button>
+              <button  class="btn-invite-ok noselect" @click="addMember">Ok</button>
             </div>
           </div>
           
@@ -206,6 +209,7 @@ export default {
           member: this.project.member.filter(ele=>ele.uid!==uid),
           postit: this.postits
         }) 
+        
     },
     removeInvite (uid) {
        this.$db.collection('project').doc(this.project.pid).update({
